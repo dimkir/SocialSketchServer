897b33ad7020d810ea7b9e0072431322e512724d
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-10-24 19:03:08 +0100

    adds putting process to background

---
+++ rbg.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# This script actually runs server
+#java -cp bin:lib/jsoup-1.7.2.jar extractcelebrities.micro.ExtractCelebrityCodes
+nohup java -cp dist/SocialSketchServer.jar:lib/twitter4j-stream-3.0.3.jar:lib/twitter4j-core-3.0.3.jar:lib/log4j-1.2.17.jar  org.socialsketch.server.Main >> bgrunning.log &
+
+
