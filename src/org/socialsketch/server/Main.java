079af485bf2d9ce1026eb7f79751a7c75c386d05
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-10-24 12:49:04 +0100

    adds test run of streaming api

--- src/org/socialsketch/server/Main.java
+++ src/org/socialsketch/server/Main.java
@@ -1,5 +1,9 @@
 package org.socialsketch.server;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import twitter4j.TwitterException;
+
 /**
  * This is entry point to server side of the social sketch.
  * 
@@ -11,8 +15,17 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Running social sketch server test stub");
+        try {
+            // TODO code application logic here
+            System.out.println("Running social sketch server test stub");
+            PrintFilterStream.main(new String[] {});
+            
+            
+            
+            
+        } catch (TwitterException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
