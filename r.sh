75369a0f40c42327b9d3cef632f06a23dd62af3c
Dimitry EC2 <dimitry@languagekings.com>
2013-10-24 10:54:37 +0000

    Adds run script

---
+++ r.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# This script actually runs server
+#java -cp bin:lib/jsoup-1.7.2.jar extractcelebrities.micro.ExtractCelebrityCodes
+java -cp dist/SocialSketchServer.jar  org.dimitry.socialsketch.server.Main
+
+
