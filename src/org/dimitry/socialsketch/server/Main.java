1b7732e458910aa0503c135c9c34fe616c6edef6
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-10-24 11:35:49 +0100

    initial commit

---
+++ src/org/dimitry/socialsketch/server/Main.java
@@ -0,0 +1,18 @@
+package org.dimitry.socialsketch.server;
+
+/**
+ * This is entry point to server side of the social sketch.
+ * 
+ * @author Dimitry Kireyenkov <dimitry@languagekings.com>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Running social sketch server test stub");
+    }
+    
+}
