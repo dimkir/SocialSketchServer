b43fa8fa64a1632fc4f7de87384855464dbb1f0f
Dimitry K Lenovo <dimkir@gmail.com>
2013-11-02 23:46:41 +0000

    makes IQuery being able to return the resulting query as string. (PersistQuery is still a stub)

---
+++ src/org/socialsketch/server/persist/query/IQuery.java
@@ -0,0 +1,11 @@
+package org.socialsketch.server.persist.query;
+
+/**
+ *
+ * @author Dimitry Kireyenkov <dimitry@languagekings.com>
+ */
+public interface IQuery {
+
+    public String renderToString();
+    
+}
---
+++ src/org/socialsketch/server/persist/query/PersistQuery.java
@@ -0,0 +1,19 @@
+package org.socialsketch.server.persist.query;
+
+/**
+ *
+ * @author Dimitry Kireyenkov <dimitry@languagekings.com>
+ */
+public class PersistQuery implements IQuery
+{
+
+    public PersistQuery(String C_TWEET_TABLE_NAME) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String renderToString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
