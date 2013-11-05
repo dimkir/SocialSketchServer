87b07a7f7dacd5af28c51f378b209c34ba738da0
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-11-05 16:46:25 +0000

    adds screenname and timestamp fields to the table

--- src/main/resources/build_schema.sql
+++ src/main/resources/build_schema.sql
@@ -1,3 +1,8 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
 -- Database: `test`
 --
@@ -7,11 +12,16 @@
 --
 -- Table structure for table `sss_tweet_table`
 --
-
 CREATE TABLE IF NOT EXISTS `sss_tweet_table` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tweet_id` bigint(20) NOT NULL,
   `tweet` varchar(1024) NOT NULL,
+  `screenname` varchar(256) NOT NULL,
+  `timestamp` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `tweet_id` (`tweet_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+SET=latin1 AUTO_INCREMENT=1 ;
