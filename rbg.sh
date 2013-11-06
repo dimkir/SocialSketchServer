132d3e142cc48342c6aa5e2c885e9876d64f9724
Dimitry EC2 <dimitry@languagekings.com>
2013-11-06 12:41:40 +0000

    adds paramter checking and +x flag

--- rbg.sh
+++ rbg.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
+if [ $# -eq 0]
+	then
+		echo "No arguments supplied, please  specify as artument txt file with terms"
+fi
+
 # This script actually runs server
 #java -cp bin:lib/jsoup-1.7.2.jar extractcelebrities.micro.ExtractCelebrityCodes
-nohup java -cp build/libs/SocialSketchServer.jar:lib/twitter4j-stream-3.0.3.jar:lib/twitter4j-core-3.0.3.jar:lib/log4j-1.2.17.jar:lib/mysql-connector-java-5.1.26-bin.jar  org.socialsketch.server.Main >> bgrunning.log &
+nohup java -cp build/libs/SocialSketchServer.jar:lib/twitter4j-stream-3.0.3.jar:lib/twitter4j-core-3.0.3.jar:lib/log4j-1.2.17.jar:lib/mysql-connector-java-5.1.26-bin.jar  org.socialsketch.server.Main $1 >> bgrunning.log &
 
 
