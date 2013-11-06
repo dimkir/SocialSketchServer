8173cebc3910587881f54c353318a769936ad38b
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-11-06 10:58:08 +0000

    adds mystest (to connect and see version) for windows platform

---
+++ mysqltest.bat
@@ -0,0 +1,6 @@
+
+@REM This script actually runs server
+@REM #java -cp bin:lib/jsoup-1.7.2.jar extractcelebrities.micro.ExtractCelebrityCodes
+java -cp build/libs/SocialSketchServer.jar;lib/twitter4j-stream-3.0.3.jar;lib/twitter4j-core-3.0.3.jar;lib/log4j-1.2.17.jar;lib/mysql-connector-java-5.1.26-bin.jar  org.socialsketch.server.persist.Version
+
+
