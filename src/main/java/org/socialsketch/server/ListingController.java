57765e9d144d1b4fc3dd16f82170e7b1a9bf5bdf
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-11-08 19:07:29 +0000

    adds controller to make it webapp.

---
+++ src/main/java/org/socialsketch/server/ListingController.java
@@ -0,0 +1,32 @@
+package org.socialsketch.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * This is controller for getting listing.
+ * 
+ * @author Dimitry Kireyenkov <dimitry@languagekings.com>
+ */
+
+@Controller
+public class ListingController {
+    
+    /**
+     * Returns view for the listing.
+     * 
+     * @param name
+     * @param model
+     * @return 
+     */
+    @RequestMapping("/listing")
+    public String listing(@RequestParam(value="name", required=false, defaultValue="World") String name,
+                            Model model){
+        
+        model.addAttribute("name", name);
+        return "listing"; // this is the name of the view?
+        
+    }
+}
