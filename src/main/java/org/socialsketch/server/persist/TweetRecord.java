e570a5bf547aaf74b9b15b5de8c4501355e48df6
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-11-10 11:36:30 +0000

    makes constants which define names of fields match the names of columns in the SQL table, so that we can use them as spec. Also adds setters which can be chained.

--- src/main/java/org/socialsketch/server/persist/TweetRecord.java
+++ src/main/java/org/socialsketch/server/persist/TweetRecord.java
@@ -19,12 +19,19 @@ public class TweetRecord implements Serializable
     private long timeStamp;
     private Date date; // converted from timestamp.
 
+    /**
+     * These are value for the map. 
+     * // maybe we can use them as well as names for the table.
+     */
+    public static final String FT_ID =  "id";
+    public static final String FT_TWEET_ID =  "tweet_id";
+    public static final String FT_TWEET =  "tweet";
+    public static final String FT_SCREEN_NAME =  "screenname";
+    public static final String FT_TIMESTAMP =  "timestamp";
     
-    public static final String F_ID =  "id";
-    public static final String F_TWEET_ID =  "tweetId";
-    public static final String F_TWEET =  "tweet";
-    public static final String F_SCREEN_NAME =  "screenName";
-    public static final String F_TIMESTAMP =  "timeStamp";
+    /**
+     * This is not a table field
+     */
     public static final String F_DATE =  "date";
 //    public static final String F_ =  "";
     
@@ -36,11 +43,11 @@ public class TweetRecord implements Serializable
         // TODO: implement toMap
         HashMap<String, String> map = new HashMap<String, String>();
 
-        map.put(F_ID, getId());
-        map.put(F_TWEET_ID, getTweetId());
-        map.put(F_TWEET, getTweet());
-        map.put(F_SCREEN_NAME, getScreenName());
-        map.put(F_TIMESTAMP, getTimeStamp());
+        map.put(FT_ID, getId());
+        map.put(FT_TWEET_ID, getTweetId());
+        map.put(FT_TWEET, getTweet());
+        map.put(FT_SCREEN_NAME, getScreenName());
+        map.put(FT_TIMESTAMP, getTimeStamp());
         map.put(F_DATE, getDate());
         return map;
     }
@@ -65,15 +72,49 @@ public class TweetRecord implements Serializable
         return String.valueOf(timeStamp);
     }
 
-    
+    public TweetRecord setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public TweetRecord setTweetId(long tweetId) {
+        this.tweetId = tweetId;
+        return this;
+    }
+
+    public TweetRecord setTweet(String tweet) {
+        this.tweet = tweet;
+        return this;
+    }
+
+    public TweetRecord setScreenName(String screenName) {
+        this.screenName = screenName;
+        return this;
+    }
+
+    public TweetRecord setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+        updateDate(timeStamp);
+        return this;
+    }
+
     /**
      * Returns date as a string.
-     * 
-     * @return 
+     *
+     * @return
      */
     private String getDate() {
         // TODO: make it render to string of the format we want. not just random sring.
         return date.toString();
+    }
+
+    /**
+     * Updates date.
+     *
+     * @param unixepoch
+     */
+    private void updateDate(long unixepoch) {
+        date.setTime(unixepoch);
     }
     
     
