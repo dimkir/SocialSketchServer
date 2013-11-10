62be484f6611c98892441fbefc2c8cf109696247
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-11-10 21:02:29 +0000

    renames main class from Main to Application

--- r.bat
+++ r.bat
@@ -3,7 +3,7 @@
 @REM #java -cp bin:lib/jsoup-1.7.2.jar extractcelebrities.micro.ExtractCelebrityCodes
 @REM java -cp build/libs/SocialSketchServer.jar;lib/twitter4j-stream-3.0.3.jar;lib/twitter4j-core-3.0.3.jar;lib/log4j-1.2.17.jar;lib/mysql-connector-java-5.1.26-bin.jar  org.socialsketch.server.Main %1
 
-@java -cp build/libs/SocialSketchServer.jar;build/output/libs/twitter4j-core-3.0.5.jar;build/output/libs/twitter4j-stream-3.0.5.jar;build/output/libs/mysql-connector-java-5.1.26.jar;build/output/libs/log4j-1.2.17.jar  org.socialsketch.server.Main %1
+@java -cp build/libs/SocialSketchServer.jar;build/output/libs/twitter4j-core-3.0.5.jar;build/output/libs/twitter4j-stream-3.0.5.jar;build/output/libs/mysql-connector-java-5.1.26.jar;build/output/libs/log4j-1.2.17.jar  org.socialsketch.server.Application %1
 
 
 
